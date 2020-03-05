4cf966300bae46b7faa35e4c493a2e3c5c00a673
chbelan <chbelan@42.FR>
2020-03-05 13:27:23 +0100

    start

---
+++ part_1/main.py
@@ -0,0 +1,10 @@
+import turtle
+
+nb1 = 42
+print(nb1)
+
+str1 = "test_one"
+print(str1)
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.forward(120)
---
+++ part_1/sqare.py
@@ -0,0 +1,15 @@
+import turtle
+
+def sqare():
+var_turtle = turtle.Turtle()
+var_turtle.forward(100)
+var_turtle.right(90)
+var_turtle.forward(100)
+var_turtle.left(90)
+
+var_turtle.left(180)
+var_turtle.forward(100)
+var_turtle.right(90)
+var_turtle.forward(100)
+
+sqare()
